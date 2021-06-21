b36944203204980ba8bcaa004d74dae136b67187
Sean Washington <hello@seanwash.com>
2021-06-20 20:11:08 -0700

    Update firebase env vars to use the non next public versions

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -2,9 +2,9 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
 // Don't re-initialize if an app has already been initialized.
