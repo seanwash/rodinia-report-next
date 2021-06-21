06aa29d084305df56309dafe2ebfe5f01711d1a5
Sean Washington <hello@seanwash.com>
2021-06-20 20:17:39 -0700

    Remove unused context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import { AuthUserProvider } from "../contexts/AuthUser";
 import Header from "../components/Header";
 
 const queryClient = new QueryClient();
@@ -10,13 +9,11 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/*<AuthUserProvider>*/}
       <Header />
       <main className="container mx-auto p-4">
         <Component {...pageProps} />
       </main>
       <ReactQueryDevtools initialIsOpen={false} />
-      {/*</AuthUserProvider>*/}
     </QueryClientProvider>
   );
 }
