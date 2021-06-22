02b9fa6ce8a3d16ef47254a884c6f4d800837d52
Chris Griffith <cgrffit@justin.tv>
2021-06-22 09:06:58 -0700

    Add simple pagination example

--- components/StoryList/StoryList.tsx
+++ components/StoryList/StoryList.tsx
@@ -1,8 +1,23 @@
 import useFetchStories from "../../hooks/useFetchStories";
 import StoryListItem from "../StoryListItem";
+import queryString from "query-string";
+import { useEffect, useState } from "react";
 
 const StoryList: React.FC = () => {
-  const { data: stories, isLoading, isError } = useFetchStories();
+  const [page, setPage] = useState(0);
+
+  // Not sure if we want to use query params to save page — could get copied by
+  // the user and would bring them to that page instead of the beginning.
+  // Another option could be to use storage.
+  useEffect(() => {
+    const queryObj = queryString.parse(window.location.search);
+      if (queryObj.page && typeof queryObj.page === "string") {
+        setPage(parseInt(queryObj.page));
+      } 
+    }
+  ), [];
+
+  const { data, isLoading, isError } = useFetchStories(page);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,14 +27,28 @@ const StoryList: React.FC = () => {
     return <div>Error</div>;
   }
 
+  const handlePreviousButtonClick = () => {
+    setPage(page-1);
+  }
+
+  const handleNextButtonClick = () => {
+    setPage(page+1);
+  }
+
   return (
-    <ul className="bg-alabaster-300 shadow-sm rounded-sm divide-y divide-alabaster mt-8">
-      {stories?.map((story) => (
-        <li key={story.id}>
-          <StoryListItem story={story} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="bg-alabaster-300 shadow-sm rounded-sm divide-y divide-alabaster mt-8">
+        {data?.stories.map((story) => (
+          <li key={story.id}>
+            <StoryListItem story={story} />
+          </li>
+        ))}
+      </ul>
+      <div className="mt-4">
+        {page > 0 && <button onClick={handlePreviousButtonClick} className="hover:underline p-2">Previous Page</button>}
+        {data?.pages && page+1 < data.pages && <button onClick={handleNextButtonClick} className="hover:underline p-2">Next Page</button>}
+      </div>
+    </>
   );
 };
 
--- hooks/useFetchStories/index.ts
+++ hooks/useFetchStories/index.ts
@@ -1,10 +1,17 @@
 import { useQuery } from "react-query";
 import { StoryWithTopics } from "../../lib/db";
 
-export default function useFetchStories() {
-  return useQuery<StoryWithTopics[], Error>(["stories"], async () => {
-    const request = await fetch("/api/stories");
-    const { stories } = await request.json();
-    return stories;
+interface Result {
+  stories: StoryWithTopics[],
+  pages: number
+}
+
+export default function useFetchStories(page: number | undefined = 0) {
+  return useQuery<Result, Error>(["stories", page], async () => {
+    const request = await fetch(`/api/stories?page=${page}`);
+    const result = await request.json();
+    return result;
+  },{
+    keepPreviousData: true,
   });
 }
