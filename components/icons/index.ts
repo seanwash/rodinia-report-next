3b28e70a574f503002df3c868a5bfa5e922922a2
Sean Washington <hello@seanwash.com>
2021-08-14 15:42:24 -0700

    Add ShareIcon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ClockIcon } from "./ClockIcon";
 export { default as GlobeAltIcon } from "./GlobeAltIcon";
-export { default as ChevronDown } from "./ChevronDown";
+export { default as ChevronDownIcon } from "./ChevronDownIcon";
+export { default as ShareIcon } from "./ShareIcon";
